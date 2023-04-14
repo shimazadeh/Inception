90716cd58c78722e638d14c9e7abec06b01a5b1f
shima <zadeh.shima@gmail.com>
2023-04-14 12:39:54 -0400

    fixing

--- Makefile
+++ Makefile
@@ -5,25 +5,25 @@ all: inception
 ${NAME}: volume build up
 
 build:
-		docker compose -f ./srcs/docker-compose.yaml build
+		docker compose -f ./srcs/docker-compose.yml build
 volume:
 		sudo mkdir -p /home/shabibol/data/db_data
 		sudo mkdir -p /home/shabibol/data/wordpress
 
 up:
-		docker-compose -f ./srcs/docker-compose.yaml up -d
+		docker-compose -f ./srcs/docker-compose.yml up -d
 
 down:
-		docker-compose -f ./srcs/docker-compose.yaml down
+		docker-compose -f ./srcs/docker-compose.yml down
 
 clean:
-		docker-compose -f ./srcs/docker-compose.yaml down
+		docker-compose -f ./srcs/docker-compose.yml down
 		docker container prune --force
 
 fclean:
-		docker-compose -f ./srcs/docker-compose.yaml down
+		docker-compose -f ./srcs/docker-compose.yml down
 		docker container prune --force
-		sudo rm -rf /home/jecolmou/data
+		sudo rm -rf /home/shabibol/data
 		docker volume ls -qf dangling=true | xargs -r docker volume rm
 		docker network prune --force
 		docker volume prune --force
--- srcs/requirements/wordpress/conf/auto_config.sh
+++ srcs/requirements/wordpress/conf/auto_config.sh
@@ -1,11 +1,12 @@
 set -x
 if ! wp core is-installed --allow-root; then
-    wp core download --force --allow-root
-    until mysql --user=${MYSQL_USER} --password=${MYSQL_PASSWORD} --host=mariadb $WORDPRESS_DB_NAME; do
-        sleep 2
-    done
-    wp config create --dbname=$WORDPRESS_DB_NAME --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$WORDPRESS_DB_HOST --allow-root
-    wp core install --url=jecolmou.42.fr --title="Inception" --admin_user=$WORDPRESS_DB_USER --admin_password=$WORDPRESS_USER_PASSWORD --admin_email=$WORDPRESS_ROOT_MAIL --skip-email --allow-root
-    wp user create $USER1_LOGIN $WORDPRESS_USER_MAIL --user_pass=$WORDPRESS_DB_PASSWORD --role=author --allow-root
+    echo hello
+#     wp core download --force --allow-root
+#     until mysql --user=${MYSQL_USER} --password=${MYSQL_PASSWORD} --host=mariadb $WORDPRESS_DB_NAME; do
+#         sleep 2
+#     done
+#     wp config create --dbname=$WORDPRESS_DB_NAME --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$WORDPRESS_DB_HOST --allow-root
+#     wp core install --url=shabibol.42.fr --title="Inception" --admin_user=$WORDPRESS_DB_USER --admin_password=$WORDPRESS_USER_PASSWORD --admin_email=$WORDPRESS_ROOT_MAIL --skip-email --allow-root
+#     wp user create $USER1_LOGIN $WORDPRESS_USER_MAIL --user_pass=$WORDPRESS_DB_PASSWORD --role=author --allow-root
 fi;
-exec "$@"
+#exec "$@"
